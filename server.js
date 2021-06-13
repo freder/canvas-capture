732795b6d7622cd5bc742fae188858c2fa607f37
Frederic Brodbeck <der.froedi@gmail.com>
2021-06-13 14:57:59 +0200

    log file names

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.post('/', (req, res) => {
-	const frameNumber = req.body.frameNumber;
-	const base64Data = req.body.dataUrl.replace(/^data:image\/png;base64,/, '');
-	const fileName = String(frameNumber).padStart(5, '0') + '.png';
+	const { frameNumber, dataUrl } = req.body;
+	const base64Data = dataUrl.replace(/^data:image\/png;base64,/, '');
+	const fileName = String(frameNumber).padStart(10, '0') + '.png';
+	console.log(fileName);
 	fs.writeFileSync(
 		path.join('output', fileName),
 		base64Data,
